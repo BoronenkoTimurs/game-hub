9401cca61830a098e52abd59310f9bb84af63a62
timurcsdd <timurubica@bk.ru>
2023-05-28 12:39:35 +0300

    Write error case for and useGame hook

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,27 +1,9 @@
-import React, { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
-import {Text} from "@chakra-ui/react"
-interface Game {
-  id: number;
-  name: string;
-}
-
-interface FatchGamesResponse {
-  count: number;
-  results: Game[];
-}
+import { Text } from "@chakra-ui/react";
+import useGames from "../hooks/useGame";
 
 const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    apiClient
-      .get<FatchGamesResponse>("/games")
-      .then((res) => setGames(res.data.results))
-      .catch((err) => setError(err.message));
-  });
-  return (
+  const {games,error}  = useGames();
+    return (
     <>
       {error && <Text>{error}</Text>}
       <ul>
