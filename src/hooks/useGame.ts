af7646aad7c78d0953b2d97edf9ccddcd10d1cfe
timurcsdd <timurubica@bk.ru>
2023-05-30 20:29:22 +0300

    Build emoji for games depens on rating

--- src/hooks/useGame.ts
+++ src/hooks/useGame.ts
@@ -13,6 +13,9 @@ export interface Game {
     background_image: string;
     parent_platforms:{platform: Platform} [];
     metacritic: number;
+    rating_top:number;
+    rating: number;
+    
   }
 const useGames = (gameQuary: GameQuary) => 
   useData<Game>('/games', {
